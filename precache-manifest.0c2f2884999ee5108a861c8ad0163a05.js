b1533cdfe5d83aab76967dc909406d359bacdad9
Amir <nik.amirsa@gmail.com>
2020-05-15 20:50:30 +0430

    Updates

---
+++ precache-manifest.0c2f2884999ee5108a861c8ad0163a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f6eb91467e024e6a081cf10aab0017",
+    "url": "/basic-cra/index.html"
+  },
+  {
+    "revision": "341f0272c73aded3206f",
+    "url": "/basic-cra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cee176eff52befabd4c0",
+    "url": "/basic-cra/static/js/2.61bd22e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-cra/static/js/2.61bd22e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341f0272c73aded3206f",
+    "url": "/basic-cra/static/js/main.3a8ee3e4.chunk.js"
+  },
+  {
+    "revision": "bd61609ae44161d04b81",
+    "url": "/basic-cra/static/js/runtime-main.909cc307.js"
+  }
+]);
